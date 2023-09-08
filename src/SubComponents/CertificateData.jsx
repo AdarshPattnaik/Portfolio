a072beef668f0b89b7357eb3d014a8843cdd7238
AdarshPattnaik <adarshme05102002@gmail.com>
2023-09-08 11:14:45 +0530

    Certification added

--- src/SubComponents/CertificateData.jsx
+++ src/SubComponents/CertificateData.jsx
@@ -8,6 +8,13 @@ const certData = [
         date: "Sept 2023",
         credential: "https://drive.google.com/file/d/1mMl5TmCFCBvWFe1Xt4KxcagP6bqh-TjK/view?usp=sharing"
     },
+    {
+        img: HackerRank,
+        skill: "JavaScript (Basic)",
+        by: "HackerRank",
+        date: "Sept 2023",
+        credential: "https://drive.google.com/file/d/1bU1-I5Eu_Pgf-T5xtSw6-moQXONj68BC/view?usp=sharing"
+    },
 ];
 
 export default certData;
